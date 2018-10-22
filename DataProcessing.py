03832fde55c2af5337ad6767cb2524fd0ed5a6a4
Peter_Jiang <865268033@qq.com>
2018-10-22 15:08:46 +0800

    initial commit the data_processing file

--- DataProcessing.py
+++ DataProcessing.py
@@ -9,6 +9,16 @@ import numpy as np
 import os
 import time
 
+col_names_chinese = ["空气温度", "空气湿度", "土壤温度", "土壤湿度", "土壤导电率", "光照度", "风速", "风向", "土壤PH", "负氧离子浓度", \
+                     "雨量", "紫外辐射", "大气压", "光合辐射", "日照时数", "空气二氧化碳", "盐分", "水溶解氧", "水溶解二氧化碳", "土壤温度-5cm", \
+                     "土壤湿度-5cm", "土壤温度-15cm", "土壤湿度-15cm", "土壤温度-35cm", "土壤湿度-35cm", "水温度", "水PH"]
+col_names_english = ["air_temp", "air_humidity", "land_temp", "land_humidity", "land_conduct", "light", "wind_sp",
+                     "wind_direct", \
+                     "land_PH", "neg_oxy_ion_concent", "rainy_num", "ultraviolet_radiation", "atm_pres",
+                     "photosynthetic_rad", \
+                     "sunshine_hours", "air_CO2", "salt", "water_dissolved_oxy", "water_dissolved_CO2", "land_temp-5cm",
+                     "land_hum-5cm", \
+                     "land_temp-15cm", "land_hum-15cm", "land_temp-35cm", "land_hum-35cm", "water_temp", "water_PH"]
 
 class Config(object):
     """
@@ -102,20 +112,27 @@ class DataProcessing(object):
         for i,name in zip(range(1,28,1),col_new_name):
             self.single_rename_col_name(data="Data_Pro/"+str(i)+".csv",col_old_name=col_old_name,col_new_name=name)
 
+
+    def initial_state(self):
+        """
+        返回初始化状态
+        :return:
+        """
+
     def process_time(self):
         """
-        处理数据
+        将时间属性秒后面的小数点去掉，然后再将数据保存起来
         :param data:
         :return:
         """
-        for i in range(1,28,1):
+        for i,name in zip(range(1,28,1),col_names_english):
             df=pd.read_csv("Data_Pro/"+str(i)+".csv")
+            df_1=pd.DataFrame()
             temp=[time.split('.')[0] for time in list(df['measureTime'].values)]
-            df['measureTime']=temp
+            df_1['measureTime']=temp
+            df_1[name]=df[name]
             print(temp)
-            df.to_csv("Data_Pro/"+str(i)+".csv",index=None)
-            # df['measureTime']=df['measureTime'].replace(df["measureTime"].values,df['measureTime'].str[0:19])
-
+            df_1.to_csv(str(i)+".csv",index=False)
 
     def  merge_csv_by_MeasureTime(self,merge_file,on=['measureTime'],how="outer",from_csv=[]):
         """
@@ -123,16 +140,16 @@ class DataProcessing(object):
         :param merge_file:
         :param on:
         :param how:
-        :param from_csv:
+        :param from_csv：
         :return:
         """
-        pd.read_csv("Data_Pro/1.csv").to_csv("Data_Pro/"+merge_file)
+        pd.read_csv("Data_Pro/a1.csv").to_csv("Data_Pro/"+merge_file)
         for i in range(2,len(from_csv),1):
-            df=pd.read_csv("Data_Pro/"+str(i)+".csv")
+            df=pd.read_csv("Data_Pro/a"+str(i)+".csv")
             df=pd.DataFrame(df)
             merge=pd.read_csv("Data_Pro/"+merge_file)
             merge=pd.DataFrame(merge)
-            pd.merge(merge,df,on=on,how=how).to_csv("Data_Pro/"+merge_file)
+            pd.merge(merge,df,on=on,how=how).to_csv("Data_Pro/"+merge_file,index=False)
         print("merge finished.")
 
     #真正的调用函数
@@ -142,17 +159,10 @@ class DataProcessing(object):
 #主函数
 def main():
     # 常量设置
-    col_names_chinese = ["空气温度", "空气湿度", "土壤温度", "土壤湿度", "土壤导电率", "光照度", "风速", "风向", "土壤PH", "负氧离子浓度", \
-                         "雨量", "紫外辐射", "大气压", "光合辐射", "日照时数", "空气二氧化碳", "盐分", "水溶解氧", "水溶解二氧化碳", "土壤温度-5cm", \
-                         "土壤湿度-5cm", "土壤温度-15cm", "土壤湿度-15cm", "土壤温度-35cm", "土壤湿度-35cm", "水温度", "水PH"]
-    col_names_english = ["air_temp", "air_humidity", "land_temp", "land_humidity", "land_conduct", "light","wind_sp","wind_direct", \
-                         "land_PH", "neg_oxy_ion_concent", "rainy_num", "ultraviolet_radiation", "atm_pres","photosynthetic_rad", \
-                         "sunshine_hours", "air_CO2", "salt", "water_dissolved_oxy", "water_dissolved_CO2","land_temp-5cm","land_hum-5cm", \
-                         "land_temp-15cm", "land_hum-15cm", "land_temp-35cm", "land_hum-35cm", "water_temp", "water_PH"]
     dp=DataProcessing()
     list_csv=os.listdir("Data_Pro")
     # dp.mul_rename_col_name(col_old_name="measureData",col_new_name=col_names_english)
-    # dp.merge_csv_by_MeasureTime("merge.csv",list_csv)
+    # dp.merge_csv_by_MeasureTime("merge.csv",on=['measureTime'],how="outer",from_csv=list_csv)
     # for i in range(3,28,1):
     #     df = pd.read_csv("Data_Pro/"+str(i)+".csv")
     #     df = pd.DataFrame(df)
@@ -160,12 +170,12 @@ def main():
     #     merge = pd.DataFrame(merge)
     #     merge_1 = pd.merge(merge, df, on=['measureTime'], how="outer")
     #     merge_1.to_csv("Data_Pro/merge.csv", index=False)
-    dp.process_time()
-    # print(pd.read_csv("Data_Pro/1.csv"))
+    # dp.process_time()
+    # for i in range(1,28,1):
+    #     df = pd.read_csv(str(i)+".csv")
+    #     df.sort_values(by='measureTime').to_csv('a'+str(i)+'.csv', index=False)
+    print(pd.read_csv("Data_Pro/merge.csv"))
 
-
-    # data=pd.read_csv("Data_Pro/merge.csv")
-    # print(data)
 if __name__=="__main__":
     main()
 
