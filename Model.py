c64185d264967cfeffc4d1c2e29d1638810d4809
Peter_Jiang <865268033@qq.com>
2018-10-23 20:12:17 +0800

    initial commit the data_processing file

---
+++ Model.py
@@ -0,0 +1,39 @@
+"""-*- coding: utf-8 -*-
+ DateTime   : 2018/10/14 16:07
+ Author  : Peter_Bonnie
+ FileName    : Model.py
+ Software: PyCharm
+"""
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import numpy as np
+from DataProcessing import *
+import tensorflow as tf
+from tensorflow.contrib.grid_rnn import Grid2LSTMCell
+from tensorflow.contrib.rnn import static_rnn
+from tensorflow.contrib import slim as slim
+
+"""
+使用Grid LSTM进行多特征预测
+2018.10。23-2018.10.27
+1.看能否把GridLSTM实现下
+2.将数据处理弄好，这样才有针对性的进行实验
+"""
+
+
+#导入相应的数据处理
+
+def Model():
+    pass
+
+
+
+def main():
+    pass
+
+
+
+if __name__=="__main__":
+
+    main()
+
---
+++ const.py
@@ -0,0 +1,14 @@
+"""-*- coding: utf-8 -*-
+ DateTime   : 2018/10/23 20:09
+ Author  : Peter_Bonnie
+ FileName    : const.py
+ Software: PyCharm
+"""
+"""
+进行常量设置
+"""
+import numpy as np
+import os
+
+DATA_ORG_PATH="Data_Orginal/"
+DATA_PRO_PATH="Data_Pro/"
